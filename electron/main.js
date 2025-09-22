beeed030b5631e93ce12124498588c82291584cf
moiz <abdulmoizali321@gmail.com>
2025-09-23 00:23:06 +0500

    changes

--- electron/main.js
+++ electron/main.js
@@ -3361,7 +3361,7 @@ function createWindow() {
     });
     // Load app
     // if (process.env.NODE_ENV === 'development') {
-    if (true) {
+    if (false) {
         mainWindow.loadURL('http://localhost:5173');
         mainWindow.webContents.openDevTools();
     } else {
